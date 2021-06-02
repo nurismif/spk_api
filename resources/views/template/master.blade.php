0bf006315f2ad879dbf666ef654e22ed0f6d6784
Aditya Putra <adityaaputraaa98@gmail.com>
2021-06-02 17:27:24 +0700

    remove unnecessary codes

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -99,17 +99,15 @@
     <!-- Bootstrap 4 -->
     {{-- <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script> --}}
 
-    <!-- DataTables  & Plugins -->
+    <!-- DataTables -->
     <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('adminlte/plugins/jszip/jszip.min.js') }}"></script>
-    <script src="{{ asset('adminlte/pdfmake/pdfmake.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('adminlte/plugins/atatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
     <!-- daterangepicker -->
     <script src="{{ asset('adminlte/plugins/moment/moment.min.js') }}"></script>
@@ -117,6 +115,9 @@
     </script>
     <!-- AdminLTE App -->
     <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+    {{-- Others --}}
+    <script src="{{ asset('adminlte/plugins/jszip/jszip.min.js') }}"></script>
+    {{-- <script src="{{ asset('adminlte/pdfmake/pdfmake.min.js') }}"></script> --}}
     <!-- Page specific script -->
     <script>
         $(function() {
