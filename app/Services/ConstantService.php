0301476946076741413234429b3a91c92422255f
Aditya Putra <adityaaputraaa98@gmail.com>
2021-06-04 17:30:54 +0700

    update the data structure of the constant data

--- app/Services/ConstantService.php
+++ app/Services/ConstantService.php
@@ -2,47 +2,25 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Collection;
+
 class ConstantService
 {
      public function getPerbandinganRules()
      {
-          return [
-               (object)[
-                    "nama" => "Sama Penting",
-                    "nilai" => "1"
-               ],
-               (object)[
-                    "nama" => "Mendekati cukup penting",
-                    "nilai" => "2"
-               ],
-               (object)[
-                    "nama" => "Cukup Penting",
-                    "nilai" => "3"
-               ],
-               (object)[
-                    "nama" => "Mendekati lebih penting",
-                    "nilai" => "4"
-               ],
-               (object)[
-                    "nama" => "Lebih Penting",
-                    "nilai" => "5"
-               ],
-               (object)[
-                    "nama" => "Mendekati sangat lebih penting",
-                    "nilai" => "6"
-               ],
-               (object)[
-                    "nama" => "Sangat Lebih Penting",
-                    "nilai" => "7"
-               ],
-               (object)[
-                    "nama" => "Mendekati mutlak lebih penting",
-                    "nilai" => "8"
-               ],
-               (object)[
-                    "nama" => "Mutlak Lebih Penting",
-                    "nilai" => "9"
-               ],
+
+          $data = [
+               "Sama Penting" => "1",
+               "Mendekati cukup penting" => "2",
+               "Cukup Penting" => "3",
+               "Mendekati lebih penting" => "4",
+               "Lebih Penting" => "5",
+               "Mendekati sangat lebih penting" => "6",
+               "Sangat Lebih Penting" => "7",
+               "Mendekati mutlak lebih penting" => "8",
+               "Mutlak Lebih Penting" => "9"
           ];
+          $data = collect($data);
+          return $data;
      }
 }
