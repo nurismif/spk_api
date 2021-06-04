93fb5179193ee03ad1cdf81d628d31b043ca75cb
Aditya Putra <adityaaputraaa98@gmail.com>
2021-06-04 17:00:49 +0700

    add matriks kriteria table database

---
+++ database/migrations/2021_06_03_100216_create_matriks_kriteria_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatriksKriteriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matriks_kriteria', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama")->nullable();
+            $table->json("row")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matriks_kriteria');
+    }
+}
---
+++ database/seeds/MatriksKriteriaSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\MatriksKriteria;
+use Illuminate\Database\Seeder;
+
+class MatriksKriteriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $records = [
+            (object)[
+                'nama' => 'Umum',
+                'row' => '{"Umum":0,"Teman Sejawat":0,"Peserta Didik":0,"Wali Murid":0,"Dunia Industri":0}',
+            ],
+            (object)[
+                'nama' => 'Teman Sejawat',
+                'row' => '{"Umum":0,"Teman Sejawat":0,"Peserta Didik":0,"Wali Murid":0,"Dunia Industri":0}',
+            ],
+            (object)[
+                'nama' => 'Peserta Didik',
+                'row' => '{"Umum":0,"Teman Sejawat":0,"Peserta Didik":0,"Wali Murid":0,"Dunia Industri":0}',
+            ],
+            (object)[
+                'nama' => 'Wali Murid',
+                'row' => '{"Umum":0,"Teman Sejawat":0,"Peserta Didik":0,"Wali Murid":0,"Dunia Industri":0}',
+            ],
+            (object)[
+                'nama' => 'Dunia Industri',
+                'row' => '{"Umum":0,"Teman Sejawat":0,"Peserta Didik":0,"Wali Murid":0,"Dunia Industri":0}',
+            ],
+        ];
+
+        foreach ($records as $rec) {
+            MatriksKriteria::create([
+                'nama' => $rec->nama,
+                'row' => $rec->row,
+            ]);
+        }
+    }
+}
