01d37e059ac9fcd9e3e4728891d2cef51afa2bc2
RamadhanZain <rbuma8426@gmail.com>
2021-06-04 13:43:47 +0700

    teacher table

--- resources/views/guru/index.blade.php
+++ resources/views/guru/index.blade.php
@@ -19,65 +19,59 @@
         </section>
 
         <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                            <div class="row">
-                                <div class="card  w-100">
-                                    <div class="card-body">
-                                        <div class="container-table100">
-                                            <div class="wrap-table100  w-100 tableIndex">
-                                                <div class="table100 ver1 m-b-110">
-                                                    <div class="table100-head">
-                                                        <table>
-                                                            <thead>
-                                                                <tr class="row100 head">
-                                                                    <th class="cell100 column1">No</th>
-                                                                    <th class="cell100 column2">Nama</th>
-                                                                    <th class="cell100 column3">NIP</th>
-                                                                    <th class="cell100 column4">Jurusan</th>
-                                                                    <th class="cell100 column5">Action</th>
-                                                                </tr>
-                                                            </thead>
-                                                        </table>
-                                                    </div>
-                                                    <div class="table100-body js-pscroll ps ps--active-y">
-                                                        <table>
-                                                            <tbody>
-                                                                @foreach ($user as $data)
-                                                                    <tr>
-                                                                        <td class="cell100 column1">{{ $no++ }}</td>
-                                                                        <td class="cell100 column2">{{ $data->nama }}</td>
-                                                                        <td class="cell100 column3">{{ $data->nip }}</td>
-                                                                        <td class="cell100 column4">{{ $data->jurusan }}
-                                                                        </td>
-                                                                        <td class="cell100 column5">
-                                                                            <form
-                                                                                action="{{ url('admin/teacher') }}/{{ $data->id }}"
-                                                                                method="POST">
-                                                                                @csrf @method('DELETE')
-                                                                                <a href="{{ url('admin/teacher') }}/{{ $data->id . '/edit' }}"
-                                                                                    class="btn btn-warning btn-sm mr-1">
-                                                                                    <i class="fa fa-pencil"></i>
-                                                                                </a>
-                                                                                <button class="btn btn-danger btn-sm"><i
-                                                                                        class="fa fa-trash"></i></button>
-                                                                            </form>
-                                                                        </td>
-                                                                    </tr>
-                                                                @endforeach
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
+            <div class="card w-100">
+                <div class="card-header">
+                    <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="dt-buttons btn-group flex-wrap">
+                                    <div class="box p-0" style="padding-bottom: 10px;">
+                                        <div class="pull-right">
+                                            <a href="/admin/user/create" class="btn btn-primary btn-flat"
+                                                style="border-radius: 5px;">
+                                                <i class="fa fa-user-plus"></i> Create
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="card-body tableIndex">
+                    <table id="exam1" class="table table-hover table-bordered table-striped mt-8">
+                        <thead>
+                            <tr style="background-color: #4a6283; color: white;">
+                                <th style="width: 1rem">No</th>
+                                <th>Nama</th>
+                                <th>NIP</th>
+                                <th>Username</th>
+                                <th>Jabatan</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($user as $data)
+                                <tr>
+                                    <td class="centerThingsColumn">{{ $no++ }}</td>
+                                    <td>{{ $data->nama }}</td>
+                                    <td>{{ $data->nip }}</td>
+                                    <td>{{ $data->username }}</td>
+                                    <td>{{ $data->jabatan }}</td>
+                                    <td class="centerThingsColumn">
+                                        <form action="{{ url('admin/teacher') }}/{{ $data->id }}" method="POST">
+                                            @csrf @method('DELETE')
+                                            <a href="{{ url('admin/teacher') }}/{{ $data->id . '/edit' }}"
+                                                class="btn btn-warning btn-sm mr-1">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                            <button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </section>
