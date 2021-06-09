f738b1e812f8a4e057139ef401651c136d0a7db8
Aditya Putra <adityaaputraaa98@gmail.com>
2021-06-09 14:36:55 +0700

    indtall passport laravel

--- app/User.php
+++ app/User.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     // JABATAN
     const TIM_KPG_ROLE = 'Tim_PKG';
