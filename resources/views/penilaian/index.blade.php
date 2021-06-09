3df433da28c025b6ae35498be5cb5f6466c376ad
Aditya Putra <adityaaputraaa98@gmail.com>
2021-06-09 14:32:24 +0700

    revert back the modal in penilaian

--- resources/views/penilaian/index.blade.php
+++ resources/views/penilaian/index.blade.php
@@ -22,7 +22,37 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        <a>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Import Kriteria</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <form method="POST" action="{{ route('import') }}" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="file"> Choose File </label>
+                                                <table>
+                                                    <td><input type="file" name="file" class="form-control"
+                                                            style="padding-bottom: 20px;" /></td>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
 
                         <section class="content">
                             <div class="card w-100">
